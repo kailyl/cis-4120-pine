b1def73877a58ddedfd5e8aa81f960b5b0e703cf
kailyl <59887357+kailyl@users.noreply.github.com>
2023-11-10 14:43:37 -0500

    starting carousel for homepage

--- pine/src/components/Homepage.jsx
+++ pine/src/components/Homepage.jsx
@@ -1,14 +1,52 @@
 import "../style/Homepage.css"
-
-import background from "../images/football.jpeg"
+import { v4 as uuidv4 } from "uuid";
+import Card from "./homepage_carousel/Card";
+import Carousel from "./homepage_carousel/Carousel";
 
 export default function Homepage() {
+    let cards = [
+        {
+          key: uuidv4(),
+          content: (
+            <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+          )
+        },
+        {
+          key: uuidv4(),
+          content: (
+            <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+          )
+        },
+        {
+          key: uuidv4(),
+          content: (
+            <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+          )
+        },
+        {
+          key: uuidv4(),
+          content: (
+            <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+          )
+        },
+        {
+          key: uuidv4(),
+          content: (
+            <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+          )
+        }
+    ];
+    
     return (
-        <div id="background" style={{ 
-            backgroundImage: `url(${background})`
-        }}>
-            <h1 id="heading"> EAGLES WIN SUPER BOWL LVIII</h1>
-            <h3 id="subheading"> From greasy fingers to greasier poles, here’s how Philadelphia celebrated the historic victory. </h3>
+        <div className="cards">
+          <Carousel
+            cards={cards}
+            height="500px"
+            width="35%"
+            margin="0 auto"
+            offset={2}
+            showArrows={false}
+          />
         </div>
-    )
+    );
 }
