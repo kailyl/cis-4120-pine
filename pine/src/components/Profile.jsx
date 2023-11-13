62498c2be86294c0e03a3e7042a598ff1ad1ef41
ANNA VAZHAEPARAMBIL <anna.vazhaeparambil@gmail.com>
2023-11-13 13:25:12 -0500

    profile page updated

--- pine/src/components/Profile.jsx
+++ pine/src/components/Profile.jsx
@@ -1,14 +1,28 @@
-import "../style/Trending.css"
+import "../style/Profile.css"
 
 import NavBar from "./NavBar";
 
-import eagles from "../images/eagles.png"
-import gfire from "../images/green-fire-icon.png"
+import cover from "../images/football.jpeg"
 
 export default function Profile() {
     return (
-        <div id="trending">
-            <h1 id="trending-heading"> PROFILE</h1>
+        <div id="profile">
+            <h1 id="profile-heading"> PROFILE</h1>
+
+            <div id="profile-image">
+                <img id="image" src={cover} height="80%" width="85%" style={{borderTopLeftRadius: '20px', borderTopRightRadius: '20px'}}></img>
+            </div>
+
+            <p id="profile-name">{"Peter Jones"}</p>
+            <p id="profile-email">{"mr.worldwide@gmail.com"}</p>
+
+            <h2 id="profile-subheading"> Your Interests</h2>
+            <div className="profile-search-bar">
+                <input type="text" placeholder="Search..." />
+            </div>
+
+            <h2 id="profile-subheading"> Restricted Sources</h2>
+
             <NavBar/> 
         </div>
     )
